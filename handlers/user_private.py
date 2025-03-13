eab0be065868a9365af45f8a16b8271dd72a16df
d.krasnov <krasnov.d.a.20@mail.ru>
2025-03-13 09:01:40 +0300

    add

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,16 +1,23 @@
-from aiogram import types, Router
-from aiogram.filters import CommandStart, Command
+from aiogram import types, Router, F
+from aiogram.filters import CommandStart, Command, or_f
+from filters.chat_types import ChatTypeFilter
+
 
 user_private_router = Router()
+user_private_router.message.filter(ChatTypeFilter(['private']))
 
 @user_private_router.message(CommandStart())
 async def start(message: types.Message):
     await message.answer('Привет')
 
-@user_private_router.message(Command('about'))
+@user_private_router.message(or_f(Command('about'), (F.text.lower() == 'о боте')))
 async def echo(message: types.Message):
     await message.answer("Информация о боте:")
 
 @user_private_router.message(Command('adress'))
 async def echo(message: types.Message):
     await message.answer("Список местоположений объектов:")
+
+# @user_private_router.message(F.text)
+# async def echo(message: types.Message):
+#     await message.answer("Список местоположений объектов:")
